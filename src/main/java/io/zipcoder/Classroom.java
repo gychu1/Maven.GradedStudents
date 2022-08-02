7e7df41172d5ed2aad125165b6c6f1e207883bc9
Git-Yee <git-yee@ZipcodersMBP58.fios-router.home>
2022-08-02 00:41:22 -0400

    worked on lab

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,57 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Map;
+
 public class Classroom {
+    Student[] students;
+    Map<Student, String> gradeBook;
+
+
+    public Classroom(int maxNumberOfStudents) {
+        this.students = new Student[maxNumberOfStudents];
+    }
+
+    public Classroom(Student[] students) {
+        this.students = students;
+    }
+
+    public Classroom() {
+        this.students = new Student[30];
+    }
+
+    public Student[] getStudents() {
+        return students;
+    }
+
+    public Double getAverageExamScore() {
+        Double average = 0.0;
+        for (int i = 0; i < students.length; i++) {
+            average += students[i].getAverageExamScore();
+        }
+       return average/students.length;
+    }
+
+    public void addStudent(Student student) {
+        Arrays.fill(students, student);
+    }
+
+    public void removeStudent(String firstName, String lastName) {
+
+        for (int i = 0; i<students.length; i++) {
+            if (students[i].firstName.equals(firstName) && students[i].lastName.equals(lastName)){
+
+            }
+        }
+
+    }
+
+    public Student[] getStudentsByScore() {
+        return null;
+    }
+
+    public String getGradeBook(){
+        return  null;
+    }
 }
--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,69 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
+    String firstName;
+    String lastName;
+    ArrayList<Double> examScores;
+
+    Student(String firstName, String lastName, Double[] examScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = new ArrayList<>();
+        this.examScores.addAll(List.of(examScores));
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getNumberOfExamsTaken() {
+        return examScores.size();
+    }
+
+    public String getExamScores(){
+        String listOfExamScores = "Exam Scores: \n";
+        for (int i = 0; i < examScores.size(); i++) {
+            listOfExamScores = listOfExamScores +
+                    "\t Exam " + (i+1) +" -> " + (examScores.get(i).intValue()) + "\n";
+        }
+        return listOfExamScores;
+    }
+
+    public void addExamScore(double examScore) {
+        examScores.add(examScore);
+    }
+
+    public void setExamScore(int examNumber, double newScore) {
+        examScores.set(examNumber-1, newScore);
+    }
+
+    public Double getAverageExamScore() {
+        Double average = 0.0;
+        for (int i = 0; i < examScores.size(); i++) {
+            average += examScores.get(i);
+        }
+        return  average/examScores.size();
+    }
+
+    @Override
+    public String toString() {
+        return "Student Name: " + firstName + " " + lastName + "\n" +
+                "> Average Score: " + getAverageExamScore().intValue() + "\n" +
+                "> " + getExamScores();
+    }
 }
